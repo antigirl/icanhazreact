7422fd29970d6e8d147dd3341113e6cbfbcf7111
Farzad Qasim <qasimf@tcd.ie>
2015-02-17 12:55:51 +0000

    abstracting out flux

--- flux/js/main.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var MyComponent = React.createClass({
-	render: function() {
-		return <div> hey buddy </div>;
-	}
-});
-
-React.render(<MyComponent /> , document.body);
